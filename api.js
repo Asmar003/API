08e082a4dff41f45a433d1c96d44d5f650eac809
Asmar003 <esmer.ceferova24@gmail.com>
2022-11-12 20:38:10 +0400

    axios

--- api.js
+++ api.js
@@ -1,28 +1,53 @@
 // https://northwind.vercel.app/api/customers/
 
+//const tBodyElement = document.querySelector('tbody');
+
+//fetch("https://northwind.vercel.app/api/customers/")
+// .then(function(response){
+//    return response.json();
+// })
+//  .then(function(data){
+//    console.log(data);
+//    for(let i=0;i<data.length;i++){
+//        const trElement = document.createElement('tr');
+//        const tdIDElement = document.createElement('td');
+//        const tdComNElement = document.createElement('td');
+//        const tdConNElement = document.createElement('td');
+//        const tdCTElement = document.createElement('td');
+//        const tdAdress1Element = document.createElement('td');
+//        const tdAdress2Element = document.createElement('td');
+//        tdIDElement.textContent=data[i].id;
+//        tdComNElement.textContent=data[i].companyName;
+//        tdConNElement.textContent=data[i].contactName;
+//        tdCTElement.textContent=data[i].contactTitle;
+//        tdAdress1Element.textContent=data[i].address.city;
+//        tdAdress2Element.textContent=data[i].address.country;
+//        trElement.append(tdIDElement,tdComNElement,tdConNElement,tdCTElement,tdAdress1Element,tdAdress2Element);
+//        tBodyElement.appendChild(trElement);
+//    }
+//  });
+
+
 const tBodyElement = document.querySelector('tbody');
 
-fetch("https://northwind.vercel.app/api/customers/")
- .then(function(response){
-    return response.json();
- })
-  .then(function(data){
-    console.log(data);
-    for(let i=0;i<data.length;i++){
-        const trElement = document.createElement('tr');
-        const tdIDElement = document.createElement('td');
-        const tdComNElement = document.createElement('td');
-        const tdConNElement = document.createElement('td');
-        const tdCTElement = document.createElement('td');
-        const tdAdress1Element = document.createElement('td');
-        const tdAdress2Element = document.createElement('td');
-        tdIDElement.textContent=data[i].id;
-        tdComNElement.textContent=data[i].companyName;
-        tdConNElement.textContent=data[i].contactName;
-        tdCTElement.textContent=data[i].contactTitle;
-        tdAdress1Element.textContent=data[i].address.city;
-        tdAdress2Element.textContent=data[i].address.country;
-        trElement.append(tdIDElement,tdComNElement,tdConNElement,tdCTElement,tdAdress1Element,tdAdress2Element);
-        tBodyElement.appendChild(trElement);
-    }
-  });
+axios("https://northwind.vercel.app/api/customers/").then(function(response){
+  console.log(response);
+
+  for(let i=0;i<response.data.length;i++){
+    const trElement = document.createElement('tr');
+    const tdIDElement = document.createElement('td');
+    const tdComNElement = document.createElement('td');
+    const tdConNElement = document.createElement('td');
+    const tdCTElement = document.createElement('td');
+    const tdAdress1Element = document.createElement('td');
+    const tdAdress2Element = document.createElement('td');
+    tdIDElement.textContent=response.data[i].id;
+    tdComNElement.textContent=response.data[i].companyName;
+    tdConNElement.textContent=response.data[i].contactName;
+    tdCTElement.textContent=response.data[i].contactTitle;
+    tdAdress1Element.textContent=response.data[i].address.city;
+    tdAdress2Element.textContent=response.data[i].address.country;
+    trElement.append(tdIDElement,tdComNElement,tdConNElement,tdCTElement,tdAdress1Element,tdAdress2Element);
+    tBodyElement.appendChild(trElement);
+  }
+});
